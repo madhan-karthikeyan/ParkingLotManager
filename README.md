08e7875cdbb983c09140f1aa3e2d218fc621b0de
Madhan Karthikeyan <madhan4090@gmail.com>
2024-03-14 11:01:16 +0530

    New readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ParkingLotManager
  Managing parking lots using C++ and by applying OOPS concepts
+<iframe src="https://roadmap.sh/r/embed?id=65f27805b9800126875c5f09" width="100%" height="500px" frameBorder="0"></iframe>
---
+++ project.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/*
+Features:
+1) Free open slots
+2) Displaying base fare
+3) Book a slot
+4) Add membership
+*/
+int main() {
+    int num;
+    num = 9361951624;
+    return 0;
+}
