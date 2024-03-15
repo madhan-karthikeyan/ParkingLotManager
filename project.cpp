8bc81802955a5518056ce44a99332b48b79e073b
Madhan Karthikeyan <madhankrthik@gmail.com>
2024-03-15 22:16:16 +0530

    Added New Class

--- project.cpp
+++ project.cpp
@@ -3,55 +3,112 @@ using namespace std;
 /*
 Features:
 1) Add User
-2) Register Car
-3) Park Car
-4) Check if car is parked
+2) Register Vehicle
+3) Park Vehicle
+4) Check if Vehicle is parked
 5) Display fare
 6) Display user details
-7) Display car details
+7) Display Vehicle details
 8) Add amount to wallet
 */
 
 class ParkingLot {
-    public:
-        int CarIn;
-        int CarOut;
+public:
+    int VehicleIn;
+    int VehicleOut;
 };
 
-class Car {
-    private:
-        int CarId;
-        string LicenseId;
-        string CarModel;
-        string CarColor;
-        string CarType;
-        bool Membership;
-        bool parked;
-        string SlotNo;
+class Vehicle {
+private:
+    int VehicleId;
+    string LicenseId;
+    string VehicleModel;
+    string VehicleColor;
+    string VehicleType;
+    bool Membership;
+    bool parked;
+    string SlotNo;
+};
+
+class Car : private Vehicle {
+private:
+    const int baseFare=200;
+    const int hourlyFare=50;
+};
+
+class Bike : private Vehicle {
+private:
+    const int baseFare=100;
+    const int hourlyFare=25;
 };
 
 class Slot {
-    private:
-        string SlotNo;
-        bool Vacancy;
-        int RemainingTime;
+private:
+    string SlotNo;
+    bool Vacancy;
+    int RemainingTime;
 };
 
 class Customer {
-    private: 
-        int CustomerId;
-        string Name;
-        int Age;
-        char Gender;
-        long int PhoneNumber;
-        int WalletAmount;
-        int CarsRegistered;
-        bool MemberShip;
-        char MembershipType;
+private: 
+    int CustomerId;
+    string Name;
+    int Age;
+    char Gender;
+    long int PhoneNumber;
+    int WalletAmount;
+    int VehiclesRegistered;
+    bool MemberShip;
+    char MembershipType;
+public:
+    void addUser() {
+        string op;
+        cout << "Enter name: ";
+        getline(cin, Name);
+        cout << "Create a User ID: ";
+        cin >> CustomerId;
+        cout << "Enter age: ";
+        cin >> Age;
+        cout << "Enter Gender (M or F): ";
+        cin >> Gender;
+        cout << "Enter Phone number: ";
+        cin >> PhoneNumber;
+        cout << "Add amount to wallet: ";
+        cin >> WalletAmount;
+        // cout << "No of : ";
+        // cin >> Name;
+        cout << "Need Membership? (y or n) ";
+        cin >> op;
+        if (op=="y") {
+            cout << "Enter Membership type: (Weekly(w) or Monthly(m) or Yearly(y)): ";
+            cin >> MembershipType;
+            MemberShip = true;
+        } else {
+            MemberShip = false;
+        }
+        
+    }
+
+    void displayUser() {
+        cout << "ID: " << CustomerId << endl;
+        cout << "Name: " << Name << endl;
+        cout << "Age: " << Age << endl;
+        cout << "Gender: " << Gender << endl;
+        cout << "Phone number: " << PhoneNumber << endl;
+        cout << "Balance: " << WalletAmount << endl;
+        if (MemberShip==true) {
+            cout << "Membership: " << "Yes" << endl;
+            cout << "Membership type: " << MembershipType << endl;
+        } else {
+            cout << "Membership: " << "No" << endl;
+        }
+
+    }
 };
 
 int main() {
-    int num;
-    
+    Customer c;
+    c.addUser();
+    c.displayUser();
     return 0;
 }
