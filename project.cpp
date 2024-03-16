dcdb86f132fd29ecd43ebcce40b3cfce9275379c
Madhan Karthikeyan <madhankrthik@gmail.com>
2024-03-16 09:50:52 +0530

    Added menu function

--- project.cpp
+++ project.cpp
@@ -24,25 +24,25 @@ private:
     string LicenseId;
     string VehicleModel;
     string VehicleColor;
-    string VehicleType;
     bool Membership;
     bool Parked;
     string SlotNo;
 public:
+    string VehicleType;
     void addVehicle();
     void displayVehicle();
 };
 
 class Car : private Vehicle {
-private:
-    const int baseFare=200;
-    const int hourlyFare=50;
+public:
+    const float baseFare=200.0;
+    const float hourlyFare=50.0;
 };
 
 class Bike : private Vehicle {
-private:
-    const int baseFare=100;
-    const int hourlyFare=25;
+public:
+    const float baseFare=100.0;
+    const float hourlyFare=25.0;
 };
 
 // Vehicle Class functions here...
@@ -60,6 +60,8 @@ void Vehicle::addVehicle() {
     cin >> op;
     if (op=="y") {
        Membership = true;
+    } else {
+        Membership = false;
     }
 }
 
@@ -73,7 +75,7 @@ void Vehicle::displayVehicle() {
     } else {
         cout << "Membership: " << "No" << endl;
     }
-
+    cout << endl;
     // if (Parked==true) {
     //     cout << "Vehicle is parked: " << "Yes" << endl;
     // } else {
@@ -84,10 +86,28 @@ void Vehicle::displayVehicle() {
 
 class Slot {
 private:
-    string SlotNo;
+    int SlotNo;
     bool Vacancy;
     int RemainingTime;
+public:
+    float fareCalculator(float, string);
 };
+// Slot Class functions here...
+float Slot::fareCalculator(float hours, string vType) {
+    float fare, num;
+    num = hours - 1.0;
+    Car c;
+    Bike b;
+    if (vType.compare("c")==0) {
+        fare += c.baseFare;
+        fare += (num*c.hourlyFare);
+    } else if (vType.compare("b")==0) {
+        fare += b.baseFare;
+        fare += (num*b.hourlyFare);
+    }
+    return fare;
+
+}
 
 class Customer {
 private: 
@@ -104,13 +124,14 @@ public:
     void addUser();
     void displayUser();
     void registerVehicle();
-    // void showVehilce();
+    void addAmountToWallet();
+    void showVehilce();
 };
 // Customer Class functions here...
 void Customer::addUser() {
     string op;
-    cout << "Enter name: ";
-    getline(cin, Name);
+    cout << "Enter name(without spaces): ";
+    cin >> Name;
     cout << "Enter age: ";
     cin >> Age;
     cout << "Enter Gender (M or F): ";
@@ -148,24 +169,88 @@ void Customer::displayUser() {
 
 }
 
-// void Customer::showVehilce() {
-    
-// }
+void Customer::showVehilce() {
+    for (int i=0; i<v.size(); i++) {
+        v[i].displayVehicle();
+        cout << endl;
+    }
+}
 
 void Customer::registerVehicle() {
     Vehicle vle;
     vle.addVehicle();
     v.push_back(vle);
     VehiclesRegistered++;
-    vle.displayVehicle();
+    cout << "Vehicle registered successfully!!" << endl;
 }
 
+void Customer::addAmountToWallet() {
+    int num;
+    cout << "Enter amount: ";
+    cin >> num;
+    WalletAmount += num;
+    cout << "Amount added successfully!!" << endl;
+}
+/*
+Assumptions for parking lot:
+This building has 3 floors
+each floor has 99 slots 
+With named as:
+1-99 in ground floor
+100-199 in first floor
+200-299 in second floor
+*/
 int main() {
     Customer c;
-    c.addUser();
-    c.displayUser();
-    c.registerVehicle();
+    Slot s;
+    int choice;
+    string vcle1;
+    do {
+        cout << "1) Add User" << endl;
+        cout << "2) Register Vehicle" << endl;
+        cout << "3) Deposit amount to wallet" << endl;
+        cout << "4) Calculate fare" << endl;
+        cout << "5) Show user details" << endl;
+        cout << "6) Show vehicle details" << endl;
+        cout << "7) Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            c.addUser();
+            break;
+        case 2:
+            c.registerVehicle();
+            break;
+        case 3:
+            c.addAmountToWallet();
+            break;
+        case 4:
+            float hours;
+            cout << "Enter duration(in hours): ";
+            cin >> hours;
+            cout << "Enter car(c) or bike(b): ";
+            cin >> vcle1;
+            cout << "Your fare is " << s.fareCalculator(hours, vcle1) << endl;  
+            break;
+        case 5:
+            c.displayUser();
+            break;
+        case 6:
+            c.showVehilce();
+            break;
+        case 7:
+            "Thank you, come again later :)";
+        }
 
-    // c.showVehilce();
+        if (choice==7) {
+            break;
+        }
+        cout << endl;
+
+    } while ((choice<7) && (choice>0));
+
+    
     return 0;
 }
